130fc350dbe162e41fe2c2f327ac7eeca5fa35ba
20022543 <20022543@myrp.edu.sg>
2021-07-16 00:01:35 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20022543/demodatabasecrud/EditActivity.java
@@ -0,0 +1,63 @@
+package sg.edu.rp.c346.id20022543.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        tvID = findViewById(R.id.tvID);
+        etContent = findViewById(R.id.etContent);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        Intent i = getIntent(); //take whatever that is passed from intent
+        data = (Note) i.getSerializableExtra("data"); //Note object passed from the MainActivity
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);  //create a DBHelper object
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data); //call DBHelper to update the notes to the latest Note object
+                dbh.close();
+
+                finish();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+
+                finish();
+
+            }
+        });
+
+
+
+    }
+}
+
+
